f6a9941f3a5de4831c97384afaaa6fd42c37e0a7
Nguyentan An <Nguyentan.An@ap.jll.com>
2019-04-05 15:42:29 +0700

    add another lib for mqtt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Paho} from 'ng2-mqtt/mqttws31';
+import { Subscription } from 'rxjs';
+import { MqttService, IMqttMessage } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription;
+
   
   title = 'mqtt-client';
   private client;
   // mqttbroker = 'test.mosquitto.org';
   // port='8080';
   // subscribedPath = '$SYS/#';
-  mqttbroker = 'broker.hivemq.com';
-  port='8000';
-  subscribedPath = 'testtopic/1';
+  // mqttbroker = 'broker.hivemq.com';
+  // port='8000';
+  // subscribedPath = 'testtopic/1';
+  mqttbroker = '113.161.168.2';
+  port='1883';
+  subscribedPath = '/NSS/#';
+
+  public message: string;
+
+  constructor(private _mqttService: MqttService) {
+    this.subscription = this._mqttService.observe(this.subscribedPath).subscribe((message: IMqttMessage) => {
+      this.message = message.payload.toString();
+      console.log(message);
+    });
+  }
+  
+  
+  public unsafePublish(topic: string, message: string): void {
+    this._mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+
 
   ngOnInit(): void {
     this.client = new Paho.MQTT.Client(this.mqttbroker, Number(this.port), 'mitsuruog');
   this.client.onMessageArrived = this.onMessageArrived.bind(this);
   this.client.onConnectionLost = this.onConnectionLost.bind(this);
-  this.client.connect({onSuccess: this.onConnect.bind(this)});
+  // this.client.connect({onSuccess: this.onConnect.bind(this)});
   }
 
 
@@ -41,4 +69,5 @@ export class AppComponent implements OnInit{
   
   }
 
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Observable } from 'rxjs';
+
+import {
+  IMqttMessage,
+  MqttModule,
+  IMqttServiceOptions
+} from 'ngx-mqtt';
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '113.161.168.2',
+  port: 1883,
+  path: '/',
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
